74a162ad31adebe2aeb004fcf2afadc106325eb4
romkravets <romann.kravets@gmail.com>
2019-04-16 23:04:04 +0300

    added page container blog

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener  } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  @ViewChild('stickyMenu') menuElement: ElementRef;
+
+  sticky: boolean = false;
+  elementPosition: any;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit() {
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll(el) { const windowScroll = window.pageYOffset;
+    // tslint:disable-next-line:align
+    if (windowScroll >= this.elementPosition) {
+        this.sticky = true;
+    }
+    // tslint:disable-next-line:one-line
+    else {
+      this.sticky = false;
+      }
+    }
 
 }
